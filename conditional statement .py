8a4f640588a678450ff687709871022744c99088
Abdullah Al Emon <emon24103076@gmail.com>
2026-02-17 20:17:08 +0600

    Add files via upload
    Signed-off-by: Abdullah Al Emon <emon24103076@gmail.com>

---
+++ conditional statement .py
@@ -0,0 +1,11 @@
+a = int(input("Enter you age : "))
+if(a>18):
+    print("You are an adult ")
+
+elif(a<0):
+    print("Your age is below 18 ")
+elif(a==18):
+    print("This is your perfect age ")
+
+else:
+    print(" Invalid ")
